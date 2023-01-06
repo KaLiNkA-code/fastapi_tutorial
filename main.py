b9e74225a70fddb4e4dcbf5bee9cf9266fe8ca48
Egor <777egorkalinin777@gmail.com>
2023-01-06 13:29:23 +0300

    part 2

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+@app.get("/user")
+async def rick_morty_read():
+    return ['Rick', 'Morty']
+
+
+@app.get("/user")
+async def no_rick_morty_read():
+    return ['Pick', 'Mick']
