7789c6ec0511686013449270af77ea8bbbc10b80
Egor <777egorkalinin777@gmail.com>
2023-01-06 16:29:01 +0300

    Part 4

---
+++ query_parameters.py
@@ -0,0 +1,65 @@
+from typing import Union
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int=0, limit: int=10):
+    return fake_items_db[skip: skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None):
+    if q:
+        return {'item_id': item_id, 'q': q}
+    return {'item_id': item_id}
+
+
+@app.get("/item/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None, short: bool=False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+        
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool=False
+    ):
+    item = {"item_id": item_id, "owner_id": user_id}
+    
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
+
+@app.get("/itemss/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
