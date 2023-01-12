a39adb74d36a6c5a8b0dadc15aeeee0d5233af84
Egor <777egorkalinin777@gmail.com>
2023-01-13 00:01:19 +0300

    part 13

---
+++ fastapi_docs/cookie_parameters.py
@@ -0,0 +1,13 @@
+"""
+Вы можете определить параметры Cookie так же, как Queryи Pathпараметры.
+"""
+from typing import Union
+from fastapi import Cookie, FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(ads_id: Union[str, None] = Cookie(default=None)):
+    return {"ads_id": ads_id}
